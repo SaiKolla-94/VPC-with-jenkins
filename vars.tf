88ed1a0e0aa5e08be8b8003d6d5a4ace438df8e7
SaiKolla <56868273+SaiKolla-94@users.noreply.github.com>
2021-10-13 13:31:34 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,3 +1,13 @@
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
+
+varible "Access_key_ID" {
+   default = "AKIAXLITOY2ZFVC2ZPW5"
+}
+
+varible "Secret_access_key" {
+   default = "Dmt6db71cXBUT2F99FxJSsA2jweDpJaMZSL6/ArF"
+}
+
+
