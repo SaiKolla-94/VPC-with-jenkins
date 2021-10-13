272fa7d9c3975a12af07e15d92cc667ba473d44a
SaiKolla <56868273+SaiKolla-94@users.noreply.github.com>
2021-10-13 13:50:40 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region = var.AWS_REGION
 }
-Access_key_ID = var.(Access_key_ID)
-Secret_access_key = var.(Secret_access_key)
+Access_key_ID = var.Access_key_ID
+Secret_access_key = var.Secret_access_key
 
